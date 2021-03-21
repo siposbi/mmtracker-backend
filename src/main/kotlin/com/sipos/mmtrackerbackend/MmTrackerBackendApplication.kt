121db42a39f6ee7c760b098123b13d78f5d1fca4
Balázs Sipos <thesiposthe@gmail.com>
2021-03-21 18:45:17 +0100

    Initial commit

---
+++ src/main/kotlin/com/sipos/mmtrackerbackend/MmTrackerBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sipos.mmtrackerbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MmTrackerBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MmTrackerBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/sipos/mmtrackerbackend/MmTrackerBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sipos.mmtrackerbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MmTrackerBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
