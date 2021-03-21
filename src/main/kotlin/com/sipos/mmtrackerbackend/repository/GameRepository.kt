bd450be85fbaa691dd9e970711b633cdd82fcce4
Balázs Sipos <thesiposthe@gmail.com>
2021-03-21 20:21:20 +0100

    Implemented repositories

---
+++ src/main/kotlin/com/sipos/mmtrackerbackend/repository/GameRepository.kt
@@ -0,0 +1,8 @@
+package com.sipos.mmtrackerbackend.repository
+
+import com.sipos.mmtrackerbackend.model.Game
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface GameRepository : JpaRepository<Game, Long> {
+
+}
---
+++ src/main/kotlin/com/sipos/mmtrackerbackend/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.sipos.mmtrackerbackend.repository
+
+import com.sipos.mmtrackerbackend.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+
+}
