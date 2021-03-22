8097686de416fa4c3740090b206765a6bb4a5997
Balázs Sipos <thesiposthe@gmail.com>
2021-03-22 23:46:54 +0100

    Implemented changes

--- src/main/kotlin/com/sipos/mmtrackerbackend/controller/GameController.kt
+++ src/main/kotlin/com/sipos/mmtrackerbackend/controller/GameController.kt
@@ -1,28 +1,26 @@
 package com.sipos.mmtrackerbackend.controller
 
+import com.sipos.mmtrackerbackend.dto.GameDTORequest
 import com.sipos.mmtrackerbackend.model.Game
-import com.sipos.mmtrackerbackend.repository.GameRepository
+import com.sipos.mmtrackerbackend.service.GameService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/games")
-class GameController(val gameRepository: GameRepository) {
+class GameController(val gameService: GameService) {
 
     @GetMapping
-    fun findAll() = gameRepository.findAll()
+    fun findAll() = gameService.findAll()
 
     @PostMapping
-    fun addGame(@RequestBody game: Game) = gameRepository.save(game)
+    fun addGame(@RequestBody game: GameDTORequest) = gameService.add(game)
 
     @PutMapping("/{id}")
-    fun updateGame(@PathVariable id: Long, @RequestBody game: Game){
-        assert(game.id == id)
-        gameRepository.save(game)
-    }
+    fun updateGame(@PathVariable id: Long, @RequestBody game: Game) = gameService.getById(id)
 
     @DeleteMapping("/{id}")
-    fun deleteGame(@PathVariable id: Long) = gameRepository.deleteById(id)
+    fun deleteGame(@PathVariable id: Long, @RequestBody game: GameDTORequest) = gameService.updateById(game, id)
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long) = gameRepository.findById(id)
+    fun getById(@PathVariable id: Long) = gameService.deleteById(id)
 }
--- src/main/kotlin/com/sipos/mmtrackerbackend/service/GameService.kt
+++ src/main/kotlin/com/sipos/mmtrackerbackend/service/GameService.kt
@@ -12,6 +12,33 @@ class GameService(
     val gameRepository: GameRepository,
     val gameConverter: GameConverter
 ) {
+    fun findAll(): MutableList<GameDTOResponse> {
+        val games = gameRepository.findAll()
+        val response = mutableListOf<GameDTOResponse>()
+        games.forEach { game: Game ->
+            response.add(gameConverter.convertToResponse(game))
+        }
+        return response
+    }
+
+    fun add(game: GameDTORequest): GameDTOResponse {
+        TODO()
+    }
+
+    fun getById(id: Long): GameDTOResponse {
+        val gameToBeReturned = gameRepository.getOne(id)
+        return gameConverter.convertToResponse(gameToBeReturned)
+    }
+
+    fun updateById(game: GameDTORequest, id: Long): GameDTOResponse {
+        TODO()
+    }
+
+    fun deleteById(id: Long) {
+        gameRepository.deleteById(id)
+    }
+
+
     fun getGamesOfUser(id: Long): List<GameDTOResponse> {
         val games = gameRepository.findByUserId(id)
         val response = mutableListOf<GameDTOResponse>()
